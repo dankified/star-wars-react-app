ed16b99a320ff52b792fd00d7654b100458123b5
Patricio de la Guardia <padlgt01@gmail.com>
2018-11-15 14:57:47 -0500

    Like really finish this shit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Link, BrowserRouter} from 'react-router-dom';
 import App from './components/App';
+import About from './components/About';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Root extends React.Component {
+
+	render() {
+		return (
+			<BrowserRouter>
+				<div>
+					<div id="header">
+						<Link to="/">Home</Link>
+						<Link to="/about">About</Link>
+					</div>
+					<Route exact path="/" component={ App } />
+					<Route path="/about" component={ About } />
+				</div>
+			</BrowserRouter>
+		)
+	}
+	
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
